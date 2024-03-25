3b02c6867073f76a0a350c09d2605a67cb9d5ee7
bhushanpatil161 <bhushanpatil161@gmail.com>
2024-03-25 22:55:39 +0530

    First Commit

---
+++ 01-FUNDAMENTALS-PART-1/index.js
@@ -0,0 +1,21 @@
+/*let js = 'amazing';
+// if (js === 'amazing') alert("JavaScript is FUN!");
+
+console.log(40 + 30 - 25 + 40);
+
+let jaavscriptIsFun = true;
+
+console.log(typeof true);
+console.log(typeof jaavscriptIsFun);
+console.log(typeof 'Bhushan');
+console.log(typeof 25, '🏎'); 
+*/
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
